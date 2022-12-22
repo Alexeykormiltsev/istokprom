1e9d407b595537b2a5feeb9a45a1fa3da7d58e2e
Alexeykormiltsev <cormiltsev@yandex.ru>
2022-12-22 15:55:28 +0500

    сделал секцию product реализовал слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,36 @@
 
 
-$(function(){
+$(function () {
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 10000,
     },
-  });     
+  });
+  const slider = new Swiper('.product__slider', {
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 50000,
+    },
+    pagination: true,
+    navigation: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+
+  });
+
 });
