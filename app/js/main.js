07fbda4ccbbdbc1fda5f7ae9fcc93c061b23ac13
Alexeykormiltsev <cormiltsev@yandex.ru>
2022-12-27 18:08:25 +0500

    сделал несколько секций, исправлял косяки в слайдерах

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,27 @@
 
 
 $(function () {
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.header__slider-inner', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 7000,
     },
+    effect: 'flip',
+    flipEffect: {
+      slideShadows: false,
+    },
+
   });
   const slider = new Swiper('.product__slider, .example__slider', {
 
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-      delay: 5000,
+    effect: 'cube',
+    cubeEffect: {
+      slideShadows: false,
+      shadow: false,
     },
+
     pagination: true,
     navigation: true,
     // If we need pagination
@@ -31,6 +36,48 @@ $(function () {
       prevEl: '.prev',
     },
 
+
+  });
+
+  const customers = new Swiper('.customers__slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'cube',
+    cubeEffect: {
+      slideShadows: false,
+      shadow: false,
+    },
+    navigation: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+  });
+  const comments = new Swiper('.comments__slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'cube',
+    cubeEffect: {
+      slideShadows: false,
+      shadow: false,
+    },
+    pagination: true,
+    navigation: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
   });
 
 });
+
