f94a68b8e842bc155490ee884d9251e238b72f97
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-01-23 16:50:28 +0500

    сделал модальное окно, привязал к кнопкам на странице index.html

--- app/js/main.js
+++ app/js/main.js
@@ -106,4 +106,42 @@ $(function () {
   });
 
 });
+var modal = document.getElementById("myModal");
+
+
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+btn3.onclick = function() {
+  modal.style.display = "block";
+}
+btn4.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
