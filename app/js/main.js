ea75ac07ad23b1f1e4c5ad84ab8b080c247e2701
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-02-14 20:34:56 +0500

    начел делать прайс лист

--- app/js/main.js
+++ app/js/main.js
@@ -211,5 +211,36 @@ window.onclick = function(event) {
 }
 
 
+// Initialise Carousel
+const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
+  Dots: false,
+});
+
+// Thumbnails
+const thumbCarousel = new Carousel(document.querySelector("#thumbCarousel"), {
+  Sync: {
+    target: mainCarousel,
+    friction: 0,
+  },
+  Dots: false,
+  Navigation: false,
+  center: true,
+  slidesPerPage: 1,
+  infinite: false,
+});
+
+// Customize Fancybox
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Carousel: {
+    on: {
+      change: (that) => {
+        mainCarousel.slideTo(mainCarousel.findPageForSlide(that.page), {
+          friction: 0,
+        });
+      },
+    },
+  },
+});
+
 
 
